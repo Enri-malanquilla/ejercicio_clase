7dda3d04aab75d98c43de7b9c0254a9404abb84f
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2022-06-01 11:13:11 +0200

    actualizado

--- src/components/entregable/components/ListaUsuarios.js
+++ src/components/entregable/components/ListaUsuarios.js
@@ -9,21 +9,23 @@ export default function ListaUsuarios() {
   const [users, setUsers] = useState([]);
   const [show, setShow] = useState(false);
   const [selectUser, setSelectUser] = useState([]);
-  //const [one_user, setOne_user] = useState([]);
+  const [one_user, setOne_user] = useState([]);
 
   useEffect(() => {
     getUser(setUsers);
   }, []);
+  console.log(selectUser);
+
+  useEffect(() => {
+    const filterUser = users.filter((user) => user.name === selectUser);
+    setOne_user(filterUser);
+  }, [selectUser.length > 0]);
 
   const showUsers = () => {
     !show && setSelectUser([]);
     return setShow(!show);
   };
-  //en construccion
-  //   const oneUser = () => {
-  //     const filterUser = users.filter((user) => user.name === selectUser);
-  //     return setOne_user(filterUser);
-  //   };
+
   return (
     <section>
       <h1>lista usuarios</h1>
@@ -35,27 +37,25 @@ export default function ListaUsuarios() {
       <ul>
         {show &&
           selectUser.length < 1 &&
-          users.map((user) => {
-            return (
-              <li key={user.id}>
-                <Usuario
-                  name={user.name}
-                  username={user.username}
-                  email={user.email}
-                  web={user.website}
-                />
-              </li>
-            );
-          })}
+          users.map((user) => (
+            <li key={user.id}>
+              <Usuario
+                name={user.name}
+                username={user.username}
+                email={user.email}
+                web={user.website}
+              />
+            </li>
+          ))}
       </ul>
-      {/* {selectUser.length > 0 && (
+      {one_user.length > 0 && (
         <Usuario
           name={one_user[0].name}
           username={one_user[0].username}
           email={one_user[0].email}
           web={one_user[0].website}
         />
-      )} */}
+      )}
     </section>
   );
 }
